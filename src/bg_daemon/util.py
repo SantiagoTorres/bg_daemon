63ed025adea6c8055e77a301797e33e34744f39a
Santiago Torres <torresariass@gmail.com>
2015-03-19 19:26:45 -0400

    Adds file-digest functions to util.py

--- src/bg_daemon/util.py
+++ src/bg_daemon/util.py
@@ -7,12 +7,14 @@
 """
 import os
 import shutil
+from hashlib import sha256
 from pkg_resources import Requirement, resource_filename, resource_string
 
 # for package-specific locations, change this value (your home folder might be
 # ideal for this)
 HOME = os.path.join(os.path.expanduser("~"), ".bg_daemon")
 PKG_LOCATION = resource_filename("bg_daemon", "")
+DIGEST_LENGTH = 10
 
 
 def initialize_default_settings(filename):
@@ -45,3 +47,46 @@ def initialize_home_directory():
     initialize_default_settings(settings_file)
 
     return
+
+
+def get_digest_for_file(filename):
+    """
+        get_digest_for_file
+
+            calculates a sha256 digest for a given file, used when backing up a
+            file. 
+            
+            In order to support legacy systems, the digest will be truncated
+            to a certain length
+
+        arguments:
+            filename: the filename of the file to obtain the digest from 
+
+        returns:
+            the a hex-encoded string containing the hash-prefix of the file
+    """
+    with open(filename) as fp:
+
+        data = fp.read()
+
+    digest = sha256(data).digest()
+
+    return hexify(digest)[:DIGEST_LENGTH]
+
+
+def hexify(byte_array):
+    """
+        hexify:
+
+        on input a byte stream, output its hex representation
+
+        arguments:
+            byte_array: a byte array to convert to hexadecimal
+
+        output:
+            the hex representation of the byte array in a string
+    """
+    return "".join("{:02x}".format(ord(c)) for c in byte_array)
+
+
+
