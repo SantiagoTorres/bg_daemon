fd5d4c5c0995c9f4af9fa10f44169ea89821c669
Santiago Torres <torresariass@gmail.com>
2015-03-27 21:12:03 -0400

    Adds automated install hook

--- setup.py
+++ setup.py
@@ -2,29 +2,52 @@
 
 import os
 from setuptools import setup
+from setuptools.command.install import install as _install
+from subprocess import call
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+class install(_install):
+    """
+        install
+
+        Custom wrapper that sets up the crontab and home folders
+
+    """
+    def run(self):
+        call(["pip install -r requirements.txt"], shell=True)
+        _install.run(self)
+
+        from bg_daemon.util import (add_crontab_entry,
+                                    initialize_home_directory)
+
+        initialize_home_directory()
+        add_crontab_entry()
+
+
 setup(
-    name = "bg_daemon",
-    version = "0.0.1",
-    author = "Santiago Torres",
-    author_email = "torresariass@gmail.com",
-    description = ("An extensible set of classes that can programatically "
-        "update the desktop wallpaper"),
-    license = "GPLv2",
-    keywords = "imgur desktop wallpapaer background",
-    url = "https://github.com/santiagotorres/bg_daemon",
-    packages = ["bg_daemon", "bg_daemon.fetchers"],
-    package_dir = {"bg_daemon": "src/bg_daemon",
-                   "bg_daemon.fetchers": "src/bg_daemon/fetchers"},
-    scripts = ["src/bg_daemon/background_daemon.py"],
-    include_package_data = True,
-    data_files = [('bg_daemon', ['src/bg_daemon/settings.json',
-                                 'src/bg_daemon/mac-update.sh'])],
-    long_description = read("README.md"),
-    classifiers = [
+    cmdclass={'install': install},
+    name="bg_daemon",
+    version="0.0.1",
+    author="Santiago Torres",
+    author_email="torresariass@gmail.com",
+    description=("An extensible set of classes that can programatically "
+                 "update the desktop wallpaper"),
+    license="GPLv2",
+    keywords="imgur desktop wallpapaer background",
+    url="https://github.com/santiagotorres/bg_daemon",
+    packages=["bg_daemon", "bg_daemon.fetchers"],
+    package_dir={"bg_daemon": "src/bg_daemon",
+                 "bg_daemon.fetchers": "src/bg_daemon/fetchers"},
+    scripts=["src/bg_daemon/background_daemon.py"],
+    include_package_data=True,
+    data_files=[('bg_daemon', ['src/bg_daemon/settings.json',
+                               'src/bg_daemon/mac-update.sh'])],
+    long_description=read("README.md"),
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Utilities",
         "License :: ",
@@ -34,11 +57,9 @@ setup(
         "Operating System :: Unix",
         "Topic :: Multimedia",
         ],
-    install_requires = [
+    install_requires=[
         "imgurpython",
         "requests",
         "python-crontab",
         ],
 )
-
-
