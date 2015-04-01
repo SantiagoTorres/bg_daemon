c9ef8a43c8d40996b3b3e909ca31cd5d6956dd5a
Santiago Torres <torresariass@gmail.com>
2015-04-01 19:26:48 -0400

    Updates test case for different width constraints

--- tests/test_imgurfetcher.py
+++ tests/test_imgurfetcher.py
@@ -5,14 +5,14 @@
     Test suite for the imgurfetcher class
 """
 import unittest
-import imgurfetcher
+import bg_daemon.fetchers.imgurfetcher as imgurfetcher
 import imgurpython
 import random
 
 NUMBER_OF_IMAGES = 5
 
 class test_imgurfetcher(unittest.TestCase):
-   
+
     fetcher = None
     gallery = None
 
@@ -23,7 +23,9 @@ class test_imgurfetcher(unittest.TestCase):
         for i in range(NUMBER_OF_IMAGES):
             self.gallery.append(imgurpython.helpers.GalleryImage(
                 link = self._generate_title(),
-                title = self._generate_title()))
+                title = self._generate_title(),
+                width = random.randint(100, 10000),
+                height = random.randint(100, 10000)))
 
     def tearDown(self):
         pass
@@ -33,18 +35,18 @@ class test_imgurfetcher(unittest.TestCase):
     Tests for:
         * input sanitation
         * a proper query string is formed
-    
-    since query strings are randomized, we don't check the actual content of 
+
+    since query strings are randomized, we don't check the actual content of
     the query, but that everything is part of the settings...
     """
     def test_build_query(self):
-   
+
         # we will test for input sanitation.
         keywords_backup = self.fetcher.keywords
         self.fetcher.keywords = None
 
         with self.assertRaises(Exception):
-            self.fetcher._build_query()    
+            self.fetcher._build_query()
 
         self.fetcher.keywords = 10
 
@@ -53,7 +55,7 @@ class test_imgurfetcher(unittest.TestCase):
 
         self.fetcher.keywords = keywords_backup
         query = self.fetcher._build_query()
-        
+
         self.assertTrue(query is not None)
         self.assertTrue(isinstance(query, str))
 
@@ -68,7 +70,7 @@ class test_imgurfetcher(unittest.TestCase):
 
             self.assertTrue(word not in self.fetcher.blacklist_words)
             self.assertTrue(is_here)
-   
+
     """
     Tests for the select image to filter filenames properly.
 
@@ -77,12 +79,12 @@ class test_imgurfetcher(unittest.TestCase):
         * Pick something that's not in the blacklist
     """
     def test_select_image(self):
-        
+
         blacklist_backup = self.fetcher.blacklist_words
         self.fetcher.blacklist_words = None
 
         self.assertTrue(self.fetcher._select_image(self.gallery) is not None)
-        
+
         self.fetcher.blacklist_words = blacklist_backup
         self.assertTrue(self.fetcher._select_image(self.gallery) is not None)
 
