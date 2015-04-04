8ad2e01a58b0e2f3d4c6fd054bb5fbfa14691a81
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-04-03 21:40:47 -0700

    ENH: Remove -i flag from quack

--- src/bg_daemon/main.m
+++ src/bg_daemon/main.m
@@ -12,23 +12,24 @@ int main(int argc, const char * argv[]) {
     NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
     BOOL backgroundChanged = NO;
 
-    backgroundPicture = [args stringForKey:@"i"] ? [args stringForKey:@"i"] : [args stringForKey:@"-input"];
-
     // ghetto way to check if help is on or not
     if ([[[NSProcessInfo processInfo] arguments] containsObject:@"-h"] ||
         [[[NSProcessInfo processInfo] arguments] containsObject:@"--help"]){
-      fprintf(stdout, "quack: update your desktop background\n");
-      fprintf(stdout, "-i (--input) - input filepath to list of frames "\
-                      "and durations\n");
+      fprintf(stdout, "quack: update your main screen's desktop background\n");
+      fprintf(stdout, "\nUsage:\n");
+      fprintf(stdout, "quack <filename> [-h/--help]\n");
+      fprintf(stdout, "\nOptions:\n");
       fprintf(stdout, "-h (--help)  - print help text and exit\n");
       return ret;
     }
 
-    // do not execute a thing if the file input is not passed in
-    if (backgroundPicture == nil) {
-        fprintf(stderr, "The -i (--input) argument is required, pass in a "
-                "background picture.\n");
-        exit(1);
+    if (argc > 1){
+      backgroundPicture = [[NSString alloc] initWithCString:argv[1]
+                                                   encoding:NSUTF8StringEncoding];
+    }
+    else{
+      fprintf(stderr, "No image passed, not changing the desktop background\n");
+      exit(1);
     }
 
     backgroundChanged = [workspace setDesktopImageURL:[NSURL fileURLWithPath:backgroundPicture]
