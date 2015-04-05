6f31122201cc178fd11d3f843ea808dddcdb90cf
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2015-04-04 21:29:05 -0700

    ENH: Use -x and command for if statement

--- src/bg_daemon/mac-update.sh
+++ src/bg_daemon/mac-update.sh
@@ -2,7 +2,7 @@
 FN=${1}-temp.jpg
 cp $1 $FN
 
-if quack -h &>/dev/null; then
+if [ -x "$(command -v quack)" ]; then
     # OSX will think it's the same image if they are named the same
     # so trick it into thinking it's a different one
     quack $FN
